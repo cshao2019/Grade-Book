1005c517a17dbe6fdf5a2f7d8e5f99c349dcd87c
Christine <Christine@Christines-MacBook-Air.local>
2016-05-12 14:10:35 -0400

    Student average and class average

--- grade_Book.py
+++ grade_Book.py
@@ -1,4 +1,30 @@
-grades = 	[[100, 80, 93, 89],
-			 [85, 80, 90, 83],
-			 [70, 91, 88, 82],
+grade_book = 	[[100, 80, 93, 89],
+			 	 [85, 80, 90, 83],
+			 	 [70, 91, 88, 82]]
+			 
+averages = []
+for row in grade_book:
+	total = 0
+	for grade in row:
+		total += grade 
+	average = total / 4.0
+	averages.append(average)
+print(averages)
+
+#________
+
+total = 0
+counter = 0
+Newaverages = []
+for row in grade_book:
+	for grade in row:
+		total += grade 
+		counter += 1
+	average = total / counter
+	Newaverages.append(average)
+print(Newaverages)
+	
+	
+		
+		
 			 
